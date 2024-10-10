b9ef79963887fa055b655646ef4211095cf4e7ae
Lena-alghannam <147579056+Lena-alghannam@users.noreply.github.com>
2024-10-10 20:49:32 +0300

    Update Employee.java

--- CSC311Project-Phase1/src/Employee.java
+++ CSC311Project-Phase1/src/Employee.java
@@ -1,5 +1,7 @@
 // Define the Employee class to represent each node in the tree
 class Employee {
+
+    int ParentID;             // Employee's unique ID
     int id;             // Employee's unique ID
     String name;        // Employee's name
     int skillLevel;     // Employee's skill level
@@ -8,7 +10,8 @@ class Employee {
     Employee right;     // Reference to the right sibling (next subordinate of the same parent)
 
     // Constructor to initialize the employee node
-    public Employee(int id, String name, int skillLevel) {
+    public Employee(int ParentID, String name, int id , int skillLevel) {
+        this.ParentID =ParentID;
         this.id = id;
         this.name = name;
         this.skillLevel = skillLevel;
@@ -17,8 +20,75 @@ class Employee {
         this.right = null;
     }
 
+    public Employee() {
+        this.ParentID = -1;
+        this.id = -1 ;
+        this.name = "";
+        this.skillLevel = -1;
+        this.parent = null;
+        this.left = null;
+        this.right = null;
+    }
+
+    public int getParentID() {
+        return ParentID;
+    }
+
+    public void setParentID(int parentID) {
+        ParentID = parentID;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSkillLevel() {
+        return skillLevel;
+    }
+
+    public void setSkillLevel(int skillLevel) {
+        this.skillLevel = skillLevel;
+    }
+
+    public Employee getParent() {
+        return parent;
+    }
+
+    public void setParent(Employee parent) {
+        this.parent = parent;
+    }
+
+    public Employee getLeft() {
+        return left;
+    }
+
+    public void setLeft(Employee left) {
+        this.left = left;
+    }
+
+    public Employee getRight() {
+        return right;
+    }
+
+    public void setRight(Employee right) {
+        this.right = right;
+    }
+
     @Override
     public String toString() {
         return "Employee [ID=" + id + ", Name=" + name + ", SkillLevel=" + skillLevel + "]";
     }
 }
+
