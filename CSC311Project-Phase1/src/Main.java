7bbb67d85b9ba69636c46cb2900680543f15893b
AichaRi <alalimabatata@gmail.com>
2024-10-11 12:54:01 +0300

    Update Main.java
    just added a cal to the method of finding comboinations

--- CSC311Project-Phase1/src/Main.java
+++ CSC311Project-Phase1/src/Main.java
@@ -4,10 +4,18 @@ public class Main {
         Employee emp1 = new Employee(0,"Ahmad",1,10);
         Employee emp2 = new Employee(1,"Khaled",2,8);
         Employee emp3 = new Employee(1,"Bader",3,12);
+        Employee emp4 = new Employee(2,"Husam",4,6);
+        Employee emp5 = new Employee(2,"Mohammed",5,9);
+       
+
 
         orgTree.addEmployee(emp1);
         orgTree.addEmployee(emp2);
         orgTree.addEmployee(emp3);
+        orgTree.addEmployee(emp4);
+        orgTree.addEmployee(emp5);
+
+        orgTree.findAllCombinations();
 
 
 
