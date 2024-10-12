a72bbd9478c62812e0b59a72ef81f6c18dd5e8fe
Lena-alghannam <147579056+Lena-alghannam@users.noreply.github.com>
2024-10-12 21:40:17 +0300

    Update Employee.java

--- CSC311Project-Phase1/src/Employee.java
+++ CSC311Project-Phase1/src/Employee.java
@@ -1,5 +1,5 @@
 class Employee {
-    int ParentID;             // Employee's unique ID
+    int ParentID;             // Employee's parent unique ID
     int id;                   // Employee's unique ID
     String name;              // Employee's name
     int skillLevel;           // Employee's skill level
@@ -18,70 +18,21 @@ class Employee {
         this.right = null;
     }
 
-    public Employee() {///do we need this?
-        this.ParentID = -1;
-        this.id = -1;
-        this.name = "";
-        this.skillLevel = -1;
-        this.parent = null;
-        this.left = null;
-        this.right = null;
-    }
-
     public int getParentID() {
         return ParentID;
     }
 
-    public void setParentID(int parentID) {
-        ParentID = parentID;
-    }
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getSkillLevel() {
         return skillLevel;
-    }
-
-    public void setSkillLevel(int skillLevel) {
-        this.skillLevel = skillLevel;
-    }
-
-    public Employee getParent() {
-        return parent;
-    }
-
-    public void setParent(Employee parent) {
-        this.parent = parent;
-    }
-
-    public Employee getLeft() {
-        return left;
-    }
-
-    public void setLeft(Employee left) {
-        this.left = left;
-    }
-
-    public Employee getRight() {
-        return right;
-    }
-
-    public void setRight(Employee right) {
-        this.right = right;
     }
 
     @Override
